61fa521f47aa40341ffc04adb2d16406d851e762
Jethro <jethrotiago@hotmail.com>
2022-11-24 23:46:41 -0300

    Criando segunda função - inserir número

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+function insert(num) {
+    let numero = document.querySelector('#resultado').innerHTML;
+    document.querySelector('#resultado').innerHTML = numero + num;
+}
+
 function clean() {
     document.querySelector('#resultado').innerHTML = "";
 }
