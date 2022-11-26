d4779d5c1241147bf0e2262e7fb250a6d672ac5d
Jethro <jethrotiago@hotmail.com>
2022-11-25 23:12:57 -0300

    Ativando a Function calcular()

--- scripts.js
+++ scripts.js
@@ -6,3 +6,12 @@ function insert(num) {
 function clean() {
     document.querySelector('#resultado').innerHTML = "";
 }
+
+function calcular() {
+    let resultado = document.querySelector('#resultado').innerHTML;
+    if(resultado) {
+        document.querySelector('#resultado').innerHTML = eval(resultado)
+    } else {
+        document.querySelector('#resultado').innerHTML = "Nada..."
+    }
+}
